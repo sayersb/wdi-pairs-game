2b6f68bef96144733736ddd271fc2eb048442c9d
sayersb <sayersb@users.noreply.github.com>
2018-04-26 16:04:37 +0100

    suits, ranks and images added js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,28 @@ var cardThree="king";
 var cardFour="king";
 
 
-var cards=["queen","queen","king","king"];
+var cards=[
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank:"queen",
+suit:"diamonds",
+cardImage:"images/queen-of-diamonds.png"
+},
+{
+rank:"king",
+suit:"hearts",
+cardImage:"images/king-of-hearts.png"
+},
+{
+rank:"king",
+suit:"diamonds",
+cardImage:"images/king-of-diamonds.png"
+}
+];
 
 var cardsInPlay=[];
 
@@ -28,10 +49,12 @@ if(cardsInPlay.length===2){
 
 checkForMatch();
 }
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
 
-};
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit)
+};	
 
 flipCard(0);
 flipCard(2);
