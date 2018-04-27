ebf563b281ca5bf84006cd0f301a5f4f8a393426
sayersb <sayersb@users.noreply.github.com>
2018-04-27 10:28:56 +0100

    game completed for now

--- js/main.js
+++ js/main.js
@@ -8,24 +8,24 @@ var cardFour="king";
 
 var cards=[
 {
-rank: "queen",
-suit: "hearts",
-cardImage: "images/queen-of-hearts.png"
+rank: 'queen',
+suit: 'hearts',
+cardImage: 'images/queen-of-hearts.png'
 },
 {
-rank:"queen",
-suit:"diamonds",
-cardImage:"images/queen-of-diamonds.png"
+rank:'queen',
+suit:'diamonds',
+cardImage:'images/queen-of-diamonds.png'
 },
 {
-rank:"king",
-suit:"hearts",
-cardImage:"images/king-of-hearts.png"
+rank:'king',
+suit:'hearts',
+cardImage:'images/king-of-hearts.png'
 },
 {
-rank:"king",
-suit:"diamonds",
-cardImage:"images/king-of-diamonds.png"
+rank:'king',
+suit:'diamonds',
+cardImage:'images/king-of-diamonds.png'
 }
 ];
 
@@ -43,18 +43,32 @@ else{
 };
 
 
-var flipCard=function(cardId){
+var flipCard=function(){
+var cardId=this.getAttribute('data-id');
+
+console.log(cardId)
+cardsInPlay.push(cards[cardId].rank);
+
+this.setAttribute('src', cards[cardId].cardImage);
 
 if(cardsInPlay.length===2){
 
 checkForMatch();
+cardsInPlay=[];
 }
-console.log("User flipped " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
 
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit)
 };	
 
-flipCard(0);
-flipCard(2);
+var createBoard=function(){
+ for(var i =0; i<cards.length; i++){
+ 	var cardElement = document.createElement('img');
+ 	cardElement.setAttribute('src', 'images/back.png');
+ 	cardElement.setAttribute('data-id', i);
+ 	cardElement.addEventListener('click', flipCard);
+ 	document.getElementById('game-board').appendChild(cardElement);
+
+ }
+
+}
+
+createBoard();
